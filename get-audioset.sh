4586670aeb0ac5582e20ce0f8b4d9cee51ef0a92
Timothy Slade <tslade@berkeley.edu>
2020-05-31 10:07:41 -0700

    Fixed broken addresses and copy/pasted snippets I'd forgotten to update

--- get-audioset.sh
+++ get-audioset.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/bash
 # Make the data/audioset folder if it's not previously there
 mkdir -p ../data/audioset
+mkdir reference
 
 # Go get the CSVs
 curl http://storage.googleapis.com/us_audioset/youtube_corpus/v1/csv/eval_segments.csv > ../data/audioset/eval_segments.csv
@@ -16,21 +17,21 @@ echo '\t unbalanced_segments contained' $(cat ../data/audioset/unbalanced_train_
 seq  -f "+" -s '' 20; echo '\n'
 
 # Go get the QA data
-curl http://storage.googleapis.com/us_audioset/youtube_corpus/v1/qa/qa_true_counts.csv > ..data/audioset/qa_true_counts.csv
+curl http://storage.googleapis.com/us_audioset/youtube_corpus/v1/qa/qa_true_counts.csv > ../data/audioset/qa_true_counts.csv
 curl http://storage.googleapis.com/us_audioset/youtube_corpus/v1/qa/rerated_video_ids.txt > ../data/audioset/rerated_video_ids.txt
 
 # Inspect a portion of the files
 seq  -f "+" -s '' 20
 echo '\n-QA true counts- Line count:' $(cat ../data/audioset/qa_true_counts.csv | wc -l) 'records'
 echo 'A sample:'
-head -5 qa_true_counts.csv
+head -5 ../data/audioset/qa_true_counts.csv
 seq  -f "-" -s '' 20
 echo '\n-Rerated video ID- line count:' $(cat ../data/audioset/rerated_video_ids.txt | wc -l) 'records'
 echo 'A sample:'
-head -5 qa_true_counts.csv
-seq  -f "+" -s '' 20
+head -5 ../data/audioset/rerated_video_ids.txt
+seq  -f "+" -s '' 20; echo '\n'
 
 
 # Go get the ontology so we can map the codes to human-readable labels
 curl https://raw.githubusercontent.com/audioset/ontology/master/ontology.json > ../data/audioset/audioset-ontology.json
-cat  ../data/audioset/audioset-ontology.json | jq -r '.[] | [.id, .name] | @csv' > ../data/audioset/audioset-human-readable-id-mapping.csv
+cat  ../data/audioset/audioset-ontology.json | jq -r '.[] | [.id, .name] | @csv' > reference/audioset-human-readable-id-mapping.csv
