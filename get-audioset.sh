bb52db78a13d0bd45cba0486c9be89bc3b0a4885
Timothy Slade <tslade@berkeley.edu>
2020-05-31 09:49:13 -0700

    Retrieving QA files, ontology, and processing ontology

--- get-audioset.sh
+++ get-audioset.sh
@@ -1,13 +1,36 @@
 #! /usr/bin/bash
+# Make the data/audioset folder if it's not previously there
 mkdir -p ../data/audioset
+
+# Go get the CSVs
 curl http://storage.googleapis.com/us_audioset/youtube_corpus/v1/csv/eval_segments.csv > ../data/audioset/eval_segments.csv
 curl http://storage.googleapis.com/us_audioset/youtube_corpus/v1/csv/balanced_train_segments.csv > ../data/audioset/balanced_train_segments.csv
 curl http://storage.googleapis.com/us_audioset/youtube_corpus/v1/csv/unbalanced_train_segments.csv > ../data/audioset/unbalanced_train_segments.csv
 
-# Verifying the line counts of each
+# Verify the line counts of each
 seq  -f "+" -s '' 20
-echo '\nTo verify:'
+echo '\nLine counts:'
 echo '\t eval_segments contained' $(cat ../data/audioset/eval_segments.csv | wc -l) 'records'
 echo '\t balanced_segments contained' $(cat ../data/audioset/balanced_train_segments.csv | wc -l) 'records'
 echo '\t unbalanced_segments contained' $(cat ../data/audioset/unbalanced_train_segments.csv | wc -l) 'records'
+seq  -f "+" -s '' 20; echo '\n'
+
+# Go get the QA data
+curl http://storage.googleapis.com/us_audioset/youtube_corpus/v1/qa/qa_true_counts.csv > ..data/audioset/qa_true_counts.csv
+curl http://storage.googleapis.com/us_audioset/youtube_corpus/v1/qa/rerated_video_ids.txt > ../data/audioset/rerated_video_ids.txt
+
+# Inspect a portion of the files
 seq  -f "+" -s '' 20
+echo '\n-QA true counts- Line count:' $(cat ../data/audioset/qa_true_counts.csv | wc -l) 'records'
+echo 'A sample:'
+head -5 qa_true_counts.csv
+seq  -f "-" -s '' 20
+echo '\n-Rerated video ID- line count:' $(cat ../data/audioset/rerated_video_ids.txt | wc -l) 'records'
+echo 'A sample:'
+head -5 qa_true_counts.csv
+seq  -f "+" -s '' 20
+
+
+# Go get the ontology so we can map the codes to human-readable labels
+curl https://raw.githubusercontent.com/audioset/ontology/master/ontology.json > ../data/audioset/audioset-ontology.json
+cat  ../data/audioset/audioset-ontology.json | jq -r '.[] | [.id, .name] | @csv' > ../data/audioset/audioset-human-readable-id-mapping.csv
