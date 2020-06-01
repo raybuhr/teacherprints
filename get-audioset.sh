36ece842881632326d0dca2ef54eb60a8b0be045
Timothy Slade <tslade@berkeley.edu>
2020-05-31 21:09:44 -0700

    Updated demarcations to work on ubuntu bash, not MacOs

--- get-audioset.sh
+++ get-audioset.sh
@@ -9,40 +9,40 @@ curl http://storage.googleapis.com/us_audioset/youtube_corpus/v1/csv/balanced_tr
 curl http://storage.googleapis.com/us_audioset/youtube_corpus/v1/csv/unbalanced_train_segments.csv > ../data/audioset/unbalanced_train_segments.csv
 
 # Verify the line counts of each
-seq  -f "+" -s '' 20
+printf '=%.0s' {1..20}; echo
 echo '\nLine counts:'
 echo '\t eval_segments contained' $(cat ../data/audioset/eval_segments.csv | wc -l) 'records'
 echo '\t balanced_segments contained' $(cat ../data/audioset/balanced_train_segments.csv | wc -l) 'records'
 echo '\t unbalanced_segments contained' $(cat ../data/audioset/unbalanced_train_segments.csv | wc -l) 'records'
-seq  -f "+" -s '' 20; echo '\n'
+printf '=%.0s' {1..20}; echo
 
 # Go get the QA data
 curl http://storage.googleapis.com/us_audioset/youtube_corpus/v1/qa/qa_true_counts.csv > ../data/audioset/qa_true_counts.csv
 curl http://storage.googleapis.com/us_audioset/youtube_corpus/v1/qa/rerated_video_ids.txt > ../data/audioset/rerated_video_ids.txt
 
 # Inspect a portion of the files
-seq  -f "+" -s '' 20
+printf '=%.0s' {1..20}; echo
 echo '\n-QA true counts- Line count:' $(cat ../data/audioset/qa_true_counts.csv | wc -l) 'records'
 echo 'A sample:'
 head -5 ../data/audioset/qa_true_counts.csv
-seq  -f "-" -s '' 20
+printf '-%.0s' {1..20}; echo
 echo '\n-Rerated video ID- line count:' $(cat ../data/audioset/rerated_video_ids.txt | wc -l) 'records'
 echo 'A sample:'
 head -5 ../data/audioset/rerated_video_ids.txt
-seq  -f "+" -s '' 20; echo '\n'
+printf '=%.0s' {1..20}; echo
 
 # Go get the ontology so we can map the codes to human-readable labels
 curl https://raw.githubusercontent.com/audioset/ontology/master/ontology.json > ../data/audioset/audioset-ontology.json
 cat  ../data/audioset/audioset-ontology.json | jq -r '.[] | [.id, .name] | @csv' > reference/audioset-human-readable-id-mapping.csv
 
 # Inpect a portion of the ontology
-seq  -f "+" -s '' 20; echo '\n'
+printf '=%.0s' {1..20}; echo
 echo 'The raw ontology:'
 jq '[.[0], .[1]]' ../data/audioset/audioset-ontology.json
-seq  -f "-" -s '' 20; echo '\n'
+printf '-%.0s' {1..20}; echo
 echo 'The simplified mapping of IDs to human-readable labels:'
 cat reference/audioset-human-readable-id-mapping.csv | head -5
-seq  -f "+" -s '' 20; echo '\n'
+printf '=%.0s' {1..20}; echo
 
 
 # Reference URLs for portions of the code above
