8ac16baebf0250a39aa491e37ae7695f9fd7cde8
Timothy Slade <tslade@berkeley.edu>
2020-06-06 18:05:10 -0700

    Updated .yml files with fully-qualified path

--- provision-conda-envs.sh
+++ provision-conda-envs.sh
@@ -1,4 +1,4 @@
 conda update -n base -c defaults conda
-conda env create -f base-env.yml
-conda env update -f viz-env.yml -n teacherprints
-conda env update -f modeling-env.yml -n teacherprints
+conda env create -f /home/projects/teacherprints/base-env.yml
+conda env update -f /home/projects/teacherprints/viz-env.yml -n teacherprints
+conda env update -f /home/projects/teacherprints/modeling-env.yml -n teacherprints
