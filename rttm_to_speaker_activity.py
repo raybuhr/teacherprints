b136bbf7fab6518b866fbd8d3c3dfc910635d4f8
Tim Slade <tslade@ischool.berkeley.edu>
2020-06-24 13:58:05 -0700

    Added 'count' column

--- rttm_to_speaker_activity.py
+++ rttm_to_speaker_activity.py
@@ -1,50 +1,54 @@
 import copy
 import pandas as pd
 
+
 def rttm_to_speaker_activity(df, outfile=None):
     """ Given an RTTM input file, generate a dataframe structured
         to support a visualization of type 'Speaker Activity' and optionally
         export to a csv located at {outfile}
-        
+
         df = Pandas DataFrame containing a standard .rttm file
-        outfile = destination for exported CSV (incl. path, filename, extension)
+        outfile = destination for exported CSV (path, filename, extension)
     """
+
     # Check whether an outfile has been defined
     if outfile is not None:
-        export=True
-    
+        export = True
+
     # Drop the columns we don't care about from a base RTTM
     vizframe = copy.deepcopy(df) \
-                   .drop(
-                    columns=['task',
-                             'inputFile',
-                             'one',
-                             'NA_1',
-                             'NA_2',
-                             'NA_3',
-                             'NA_4'])
-    
+        .drop(
+        columns=[
+            'task',
+            'inputFile',
+            'one',
+            'NA_1',
+            'NA_2',
+            'NA_3',
+            'NA_4'])
+
     # Rename columns for our viz's purposes
     vizframe = vizframe.rename(columns={
-                                'start':'START',
-                                'duration':'DUR',
-                                'class':'LABEL'
-                                })
-    
+        'start': 'START',
+        'duration': 'DUR',
+        'class': 'LABEL'
+    })
+
     # Remap the model classes for this viz's purposes
     vizframe['LABEL'] = vizframe['LABEL'].map({
-                              'KCHI':'CHILD',
-                              'CHI':'CHILD',
-                              'FEM':'ADULT',
-                              'MAL':'ADULT'
-                              })
-    
+        'KCHI': 'CHILD',
+        'CHI': 'CHILD',
+        'FEM': 'ADULT',
+        'MAL': 'ADULT'
+    })
+
     # Filter the dataframe to just the 'clean' (non-'SPEECH') classes
     vizframe = vizframe[vizframe['LABEL'].isin(['CHILD', 'ADULT'])]
     vizframe['LABEL_NUM'] = vizframe['LABEL'] \
-                                     .apply(lambda x: 1 if x=='CHILD'
-                                                        else (-1 if x=='ADULT' else NaN))
-    vizframe['DUR_TRANS'] = vizframe['LABEL_NUM'] * vizframe['DUR']    
+        .apply(lambda x: 1 if x == 'CHILD'
+               else (-1 if x == 'ADULT' else NaN))
+    vizframe['DUR_TRANS'] = vizframe['LABEL_NUM'] * vizframe['DUR']
+    vizframe['COUNT'] = 1
 
     if export:
         vizframe.to_csv(outfile)
