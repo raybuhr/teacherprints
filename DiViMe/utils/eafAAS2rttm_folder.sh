ef906abbabb37baf1620f98c813cd204b4070f40
Anna Jacobson <annacjacobson@berkeley.edu>
2020-07-19 16:15:47 -0700

    Removed DiViMe and vandam_practice

--- DiViMe/utils/eafAAS2rttm_folder.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# Shell script to convert all .eaf files following the ACLEW Annotation Scheme inside a folder to .rttm format
-# 
-for j in ${1}/*.eaf; do
-    r=`echo $j | sed "s/.eaf/rttm/"`
-    elan2rttm.py -i $j -o $r
-#generate the basic rttms
-done
